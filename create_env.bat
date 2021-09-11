377461c31970aba79dd5abb5bc9bbb4f460cdc5a
Samuel V <realalloutmusic@gmail.com>
2021-09-11 10:24:40 -0400

    fixed paths create_env.bat

--- create_env.bat
+++ create_env.bat
@@ -1,3 +1,3 @@
 python3 -m venv venv
-Powershell.exe -executionpolicy remotesigned -File venv/bin/Activate.ps1
+Powershell.exe -executionpolicy remotesigned -File venv\bin\Activate.ps1
 pip install -r libraries.txt
