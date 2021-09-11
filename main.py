cd43007a8e767401200e778e15170a47b80a77c6
Samuel V <realalloutmusic@gmail.com>
2021-09-11 10:16:12 -0400

    Init

---
+++ main.py
@@ -0,0 +1,15 @@
+import socket
+
+from socket import UD
+
+
+def getInput():
+    return input("[L/R]: ")
+
+
+with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as s:
+    s.connect(("wpilib.local", 6001))
+    s.sendall(getInput())
+    payload = s.recv(1024)
+
+    print(payload)
